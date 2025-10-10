6b09d308cb0beb1dde050197e6f732c8de9f2c8e
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2025-10-10 21:52:21 +0300

    update the APP_BASE_URL

--- backend/admin-panel/src/services/api.js
+++ backend/admin-panel/src/services/api.js
@@ -2,8 +2,7 @@
 import axios from 'axios';
 
 // Use the deployed backend URL in production
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL ='https://dbuianfashion.onrender.com/api';
 
 
 const api = axios.create({
