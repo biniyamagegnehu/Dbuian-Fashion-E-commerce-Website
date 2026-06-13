2178b2052a214c0814747c614191aaebc2d85485
Biniyam Agegnehu <biniyamagegnehu@gmail.com>
2026-06-13 09:28:46 +0300

    feat: add role support to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -40,8 +40,8 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['customer', 'admin'],
-    default: 'customer'
+    enum: ['user', 'admin'],
+    default: 'user'
   },
   address: {
     blockNumber: String,
